836f16291e288c872e52905fe82df1819f2c7174
Karen Stolzenberg <kstolzenberg@gmail.com>
2015-07-21 19:55:19 -0700

    partial working motion - green not working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # drawbot
+
+A drawing robot built on a Raspberry Pi & [PiCy](https://www.piborg.org/picypack).
---
+++ TurtleBorgTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: Latin-1
+# from piborg.org
+
+import TurtleBorg
+
+#TurtleBorg.MoveForward(1)
+#TurtleBorg.MoveLeft(0.85)
+#TurtleBorg.MoveForward(1)
+#TurtleBorg.MoveLeft(0.85)
+#TurtleBorg.MoveForward(1)
+#TurtleBorg.MoveLeft(0.85)
+#TurtleBorg.MoveForward(1)
+#TurtleBorg.MoveLeft(0.85)
+#TurtleBorg.MoveRight(.75)
+TurtleBorg.MoveForward(2)
